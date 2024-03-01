d3d17276151af5a551f573d8d0a2f67d1624fa09
guniguni <guniguni513@gmail.com>
2024-03-01 11:02:32 +0900

    jsonの取得、表示成功

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import requests
 import json
+
+URL='https://weather.tsukumijima.net/api/forecast/city/130010'
+#通信をしてレスポンスを取得
+res = requests.get(URL)
+#レスポンスから本文(JSONデータ)を取り出して、パースする
+data=json.loads(res.text)
+#デバッグプリント
+print(data)
