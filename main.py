8e4e6293e7883025284fd25c2da47c9173392122
guniguni <guniguni513@gmail.com>
2024-03-01 13:35:38 +0900

    綺麗に見やすく

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import requests
 import json
 
 URL='https://weather.tsukumijima.net/api/forecast/city/130010'
-#レスポンスから本文(JSONデータ)を取り出して、パースする
 data=requests.get(URL).json()
 weather=[[data['forecasts'][0]['telop'],data['forecasts'][0]['temperature']['max']['celsius']]]
-#result.csvを開いて（無ければ新規に作成される）書き込む
 with open('result.csv','w',encoding='utf-8')as file:
     for w in weather:
         file.write(f'{w[0]},{w[1]}\n')
