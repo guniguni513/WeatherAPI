6122b5ab8a3b0c3b1d5be307c1a2a043802af22c
guniguni <guniguni513@gmail.com>
2024-03-01 11:55:39 +0900

    ようやくforecastsの取得に成功

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import requests
 import json
+#Weatherクラス
+class Weather:
+    #コンストラクタ
+    def __init__(self,telop):
+        self.telop=telop
+    #予報をCSV文字列にするメソッド
+    def toCSV(self):
+        return f'{self.telop}'
 
 URL='https://weather.tsukumijima.net/api/forecast/city/130010'
 #通信をしてレスポンスを取得
@@ -7,4 +15,11 @@ res = requests.get(URL)
 #レスポンスから本文(JSONデータ)を取り出して、パースする
 data=json.loads(res.text)
 #デバッグプリント
-print(data)
+#print(data)
+
+#リスト作成
+weather=[]
+#天気と最高気温を取り出してWeatherオブジェクトを作成し、リストに追加
+weather.append(Weather(data['forecasts'][2]))
+#デバッグプリント
+print(weather[0].toCSV())
