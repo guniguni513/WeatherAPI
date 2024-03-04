cbac88fb3eb9b41357e7b5af0e57c5bdb4a8d648
guniguni <guniguni513@gmail.com>
2024-03-04 12:58:49 +0900

    完成品

--- main.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-URL='https://weather.tsukumijima.net/api/forecast/city/130010'
-data=requests.get(URL).json()
-weather=[[data['forecasts'][0]['telop'],data['forecasts'][0]['temperature']['max']['celsius']]]
-with open('result.csv','w',encoding='utf-8')as file:
-    for w in weather:
-        file.write(f'{w[0]},{w[1]}\n')
